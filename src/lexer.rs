555f24de9126eddd29aaba20debe6764baf91222
Kartikeya Srivastav <kartikeya251@gmail.com>
2025-12-16 21:17:21 +0530

    Addition of Basic Lexer -> Editron.V0.1

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io;
 
 #[derive(Debug, Clone)]
 pub struct Token {
@@ -28,7 +29,7 @@ pub enum TokenKind {
     Let,
     EOF,
 }
-fn TokenToChr(c: char) -> Option<TokenKind> {
+fn char_to_token(c: char) -> Option<TokenKind> {
     match c {
         '(' => Some(TokenKind::LeftParen),
         ')' => Some(TokenKind::RightParen),
@@ -45,19 +46,27 @@ fn TokenToChr(c: char) -> Option<TokenKind> {
     }
 }
 
-fn main() {
-    let source = fs::read_to_string("input.edt");
+pub fn lexer() -> Result<Vec<Token>, io::Error> {
+    let source = fs::read_to_string("input.edt")?;
     let mut data: Vec<Token> = Vec::new();
-    let mut line;
+    let mut line = 1;
 
     for i in source.chars() {
         if i == '\n' {
             line += 1;
             continue;
         }
-        if i == ' ' && i == '\t' {
+        if i == ' ' || i == '\t' {
             continue;
         }
-        if let Some(kind) = TokenToChr(i) {}
+        if let Some(kind) = char_to_token(i) {
+            let a = Token {
+                kind,
+                value: i.to_string(),
+                line,
+            };
+            data.push(a);
+        }
     }
+    return Ok(data);
 }
