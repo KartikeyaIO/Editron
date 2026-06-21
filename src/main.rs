ee2cd2cb879168c078d7c8dc5e151150d3295e3f
Kartikeya Srivastav <kartikeya251@gmail.com>
2026-06-21 13:04:36 +0530

    Testing current Arch

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use drive::engine::Engine;
 use drive::parser;
+use std::time::Instant;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let source = std::fs::read_to_string("input.drive").expect("Failed to read file");
 
     let program = parser::parse(&source).expect("Parsing Failed!");
 
     let mut engine = Engine::new();
+    let start = Instant::now();
 
     engine.run(&program).expect("Execution Error!");
+    println!("Execution Completed in {:?}",start.elapsed());
 
     Ok(())
 }
